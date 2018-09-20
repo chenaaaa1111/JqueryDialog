dc1aacc9e48f625ab4bcbc4243afacad67d0acbe
chenaaaa1111 <710898849@qq.com>
2018-09-20 17:50:47 +0800

    dese
     <script type="text/javascript" src="js/lib.js"></script>
    
        <script type="text/javascript">
            showPromise('haha',function(){
                    alert('弹窗关闭')
            });
            showDialog({msg:'HELLO WORLS',
                makeSure:function(res){
                console.log('123');
                },
                cancen:function(){
                    alert(66)
                }
    
        })
    
        </script>

---
+++ lib.js
@@ -0,0 +1,42 @@
+
+	 function	showDialog(options){
+	 		var defaultOptions={
+	 			msg:'你好',
+	 			cancen:function(){
+
+	 			},
+	 			makeSure:function(){
+	 				alert(123);
+	 			}
+	 		}
+       options=$.extend(defaultOptions,options)
+
+	    var html='<div class="ui-dialog show">\
+			    <div class="ui-dialog-cnt">\
+			        <div class="ui-dialog-bd">\
+			            <p>'+options.msg+'</p>\
+			        </div>\
+			        <div class="ui-dialog-ft">\
+			            <button type="button" class="cancen" onclick=""  data-role="button">取消</button>\
+			            <button type="button" class=" makesure " onclick="" data-role="button">确定</button>\
+			        </div>\
+			    </div>\
+			</div>'
+	     $('body').append(html);
+         $('body').on('click','.makesure',options.makeSure);
+          $('body').on('click','.makesure',options.cancen);
+}
+	function showPromise(msg,callback){
+			  var html='<div class="ui-dialog show">\
+			    <div class="ui-dialog-cnt">\
+			        <div class="ui-dialog-bd">\
+			            <p>'+msg+'</p>\
+			        </div>\
+			        <div class="ui-dialog-ft">\
+			            <button type="button" onclick="closethis('+callback+')" data-role="button">确定</button>\
+			        </div>\
+			    </div>\
+			</div>'
+	     $('body').append(html);
+	   
+	}
